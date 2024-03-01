7a41c3787ea5bb1fc39c36f45ae1c6621315b22b
Nguyễn Võ Chí Dũng <145178800+NZodasic@users.noreply.github.com>
2024-03-01 09:53:20 +0700

    feat: Add connect fire detector via iphone

--- Code/fire.py
+++ Code/fire.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 import cvzone
 
 # Running real time from webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 model = YOLO('./Code/fire.pt')
 
 # Reading the classes
@@ -15,14 +15,13 @@ while True:
     frame = cv2.resize(frame, (640, 480))
     result = model(frame, stream=True)
 
-    # Getting bbox, confidence, and class names information to work with
     for info in result:
         boxes = info.boxes
         for box in boxes:
             confidence = box.conf[0]
             confidence = math.ceil(confidence * 100)
             Class = int(box.cls[0])
-            if confidence > 50:
+            if confidence > 70:
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 5)

ca7f30024e460393fbf18e5a5b34cd450f822d28
Nguyễn Võ Chí Dũng <145178800+NZodasic@users.noreply.github.com>
2024-05-15 03:02:34 +0700

    feat: Organize Folder

--- Code/Fire-Smoke/predict.py
+++ Code/Fire-Smoke/predict.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('fire.pt')
+model = YOLO('best.pt')
 
-model.predict(source='99.jpg', imgsz = 640, conf = 0.6, save = True)
+model.predict(source='fire2.mp4', imgsz = 640, conf = 0.8, save = True)
 
 # model.predict(source=0, imgsz = 640, conf = 0.6, save = True, show=True)
